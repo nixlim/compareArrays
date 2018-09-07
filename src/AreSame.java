6e42ed82e7f1fc4d4ac8ece5f63d805729ca06dc
Igor Ryabchuk <igor.ryabchuk@tesco.com>
2018-09-07 11:31:15 +0100

    Add test/code for checking array length

--- src/AreSame.java
+++ src/AreSame.java
@@ -1,9 +1,19 @@
 //https://www.codewars.com/kata/are-they-the-same/train/java
 
+import java.util.Arrays;
+
 public class AreSame {
 
   public static boolean comp(int[] a, int[] b) {
+    Arrays.sort(a);
+    Arrays.sort(b);
 
-    return false;
+    if (a.length == b.length && (a.length != 0)) {
+      
+      return true;
+    } else {
+      return false;
+    }
   }
+
 }
--- test/AreSameTest.java
+++ test/AreSameTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -6,19 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class AreSameTest {
 
   @Test
+  @DisplayName ("Should return true if array are same length.")
+  public void test0 () {
+    int[] a = new int[]{121, 144};
+    int[] b = new int[]{121, 14641};
+    assertEquals(true, AreSame.comp(a, b));
+  }
+
+  @Test
+  @DisplayName ("Should return false if array is empty")
+  public void test00 () {
+    int[] a = new int[0];
+    int[] b = new int[0];
+    assertEquals(false, AreSame.comp(a, b));
+  }
+
+  @Disabled
+  @Test
   @DisplayName ("Should return true")
-  public void test1() {
+  public void test1 () {
     int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
     int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
     assertEquals(true, AreSame.comp(a, b));
   }
 
+  @Disabled
   @Test
   @DisplayName("Should return false")
   public void test2() {
     int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
     int[] b = new int[]{121, 14641, 20736, 36100, 25921, 361, 20736, 361};
-    assertEquals(true, AreSame.comp(a, b));
+    assertEquals(false, AreSame.comp(a, b));
   }
 
 }
